e3bf27cfff4a48921256a6fa2671fec140272154
Gabriele K Silva <gabriele.s@aluno.ifsp.edu.br>
2024-10-13 17:27:22 -0300

    adicionando chamada ao toolbar e ao menu para aparecer na tela inicial

--- app/src/main/java/br/edu/scl/ifsp/ads/pdm/petlife/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/ads/pdm/petlife/MainActivity.kt
@@ -1,15 +1,25 @@
 package br.edu.scl.ifsp.ads.pdm.petlife
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.scl.ifsp.ads.pdm.petlife.databinding.ActivityMainBinding
+import br.edu.scl.ifsp.ads.pdm.petlife.databinding.ToolbarBinding
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy{
         ActivityMainBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+        setSupportActionBar(amb.toolbarIn.toolbar)
+
+    }
+    //criar o menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 }
