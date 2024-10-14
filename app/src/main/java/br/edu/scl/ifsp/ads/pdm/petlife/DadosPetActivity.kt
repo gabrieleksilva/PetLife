a49d85ef5c65eb74d25d87c1d51587041349cce4
Gabriele K Silva <gabriele.s@aluno.ifsp.edu.br>
2024-10-14 00:15:06 -0300

    adicionando button e deletando imports desnecessarios

--- app/src/main/java/br/edu/scl/ifsp/ads/pdm/petlife/DadosPetActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/ads/pdm/petlife/DadosPetActivity.kt
@@ -1,12 +1,8 @@
 package br.edu.scl.ifsp.ads.pdm.petlife
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import br.edu.scl.ifsp.ads.pdm.petlife.databinding.ActivityDadosPetBinding
-import br.edu.scl.ifsp.ads.pdm.petlife.databinding.ActivityMainBinding
 
 class DadosPetActivity : AppCompatActivity() {
     private val adb: ActivityDadosPetBinding by lazy{
