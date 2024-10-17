bd7d420302e4a2dacbdaa75ebaee4925589ac291
Gabriele K Silva <gabriele.s@aluno.ifsp.edu.br>
2024-10-17 00:33:33 -0300

    criação da classes de Pet e importação da dependencia Parcelable

---
+++ app/src/main/java/br/edu/scl/ifsp/ads/pdm/petlife/Pet.kt
@@ -0,0 +1,13 @@
+package br.edu.scl.ifsp.ads.pdm.petlife;
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class Pet(
+        var nome: String,
+        var dtNasc: String,
+        var tipo: String,
+        var cor: String,
+        var porte: String
+): Parcelable
