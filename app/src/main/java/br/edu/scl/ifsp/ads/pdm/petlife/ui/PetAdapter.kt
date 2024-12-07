e5c096697e6cbbd3ee0c6b31fb08495d26bef819
Gabriele K Silva <gabriele.s@aluno.ifsp.edu.br>
2024-12-07 13:54:07 -0300

    pegando o pet que sera usado para preencher a celula, verificando se a celula ja foi inflada ou nao. Se nao foi, infla uma nova celula

--- app/src/main/java/br/edu/scl/ifsp/ads/pdm/petlife/ui/PetAdapter.kt
+++ app/src/main/java/br/edu/scl/ifsp/ads/pdm/petlife/ui/PetAdapter.kt
@@ -1,9 +1,14 @@
 package br.edu.scl.ifsp.ads.pdm.petlife.ui
 
 import android.content.Context
+import android.content.Context.LAYOUT_INFLATER_SERVICE
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import br.edu.scl.ifsp.ads.pdm.petlife.R
+import br.edu.scl.ifsp.ads.pdm.petlife.databinding.TilePetBinding
 import br.edu.scl.ifsp.ads.pdm.petlife.model.Pet
 
 class PetAdapter (context: Context,
@@ -13,4 +18,25 @@ class PetAdapter (context: Context,
         val nameTv: TextView,
         val typeTv: TextView
     )
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        lateinit var tpb: TilePetBinding
+
+        val pet = petList[position]
+
+        var petTile = convertView
+
+        if (petTile == null) {
+            tpb = TilePetBinding.inflate(
+                context.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater,
+                parent,
+                false
+            )
+
+            petTile = tpb.root
+
+        }
+
+        return super.getView(position, convertView, parent)
+    }
 }
