b12b5e765414eb8c0bb9147805faf3f0445c0c8f
Gabriele K Silva <gabriele.s@aluno.ifsp.edu.br>
2024-12-08 01:16:35 -0300

    inflando um novo layout se não houver uma view reciclada para armazenar as referências dos componentes de ui

--- app/src/main/java/br/edu/scl/ifsp/ads/pdm/petlife/ui/EventAdapter.kt
+++ app/src/main/java/br/edu/scl/ifsp/ads/pdm/petlife/ui/EventAdapter.kt
@@ -1,16 +1,51 @@
 package br.edu.scl.ifsp.ads.pdm.petlife.ui
 
 import android.content.Context
+import android.content.Context.LAYOUT_INFLATER_SERVICE
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import br.edu.scl.ifsp.ads.pdm.petlife.R
+import br.edu.scl.ifsp.ads.pdm.petlife.databinding.TileEventBinding
 import br.edu.scl.ifsp.ads.pdm.petlife.model.Event
 
 class EventAdapter(context: Context,
                    private val eventList: MutableList<Event>):
     ArrayAdapter<Event>(context, R.layout.tile_event, eventList) {
-    private data class PetTileHolder(
+    private data class EventTileHolder(
         val dataTv: TextView,
         val descTv: TextView
     )
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        lateinit var teb: TileEventBinding
+
+        // pegar o evento que vai ser usado para preencher a celula
+        val event = eventList[position]
+
+        //verificando se a celula ja foi inflada ou nao
+        var eventTile = convertView
+
+        if (eventTile == null) {
+            //Se nao foi, infla uma nova celula
+            teb = TileEventBinding.inflate(
+                context.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater,
+                parent,
+                false
+            )
+
+            eventTile = teb.root
+
+            // Criar um holder para a nova celula
+            val newEventTileHolder = EventTileHolder(
+                teb.dataTv,
+                teb.descricaoTv
+            )
+
+            eventTile.tag = newEventTileHolder
+        }
+        return eventTile
+    }
 }
