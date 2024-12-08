d1a7beed06ea05bd2cc3edb83dd27f46f9691f2f
Gabriele K Silva <gabriele.s@aluno.ifsp.edu.br>
2024-12-08 09:46:28 -0300

    adicionando a listagem de events, com uma lista qualquer para teste. Adicionando o adapter event na chamada da activity

--- app/src/main/java/br/edu/scl/ifsp/ads/pdm/petlife/ui/EventListActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/ads/pdm/petlife/ui/EventListActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.scl.ifsp.ads.pdm.petlife.R
 import br.edu.scl.ifsp.ads.pdm.petlife.databinding.ActivityEventListBinding
-import br.edu.scl.ifsp.ads.pdm.petlife.model.Constant.EVENT_LIST
-import br.edu.scl.ifsp.ads.pdm.petlife.model.Pet
+import br.edu.scl.ifsp.ads.pdm.petlife.model.Event
 
 class EventListActivity : AppCompatActivity() {
     private val aelb: ActivityEventListBinding by lazy {
         ActivityEventListBinding.inflate(layoutInflater)
+    }
+    // Data source
+    private val eventList: MutableList<Event> = mutableListOf()
+    //Adapter
+    private val eventAdapter: EventAdapter by lazy {
+        EventAdapter(this, eventList)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +24,21 @@ class EventListActivity : AppCompatActivity() {
         aelb.toolbarIn.toolbar.let {
             it.subtitle = getString(R.string.event_list)
             setSupportActionBar(it)
+        }
+        fillEventList()
+        aelb.eventoLv.adapter = eventAdapter
+
+
+    }
+    private fun fillEventList(){
+        for (index in 1..10){
+            //populando a lista com valores estaticos
+            eventList.add(
+                Event(
+                    "Data evento $index",
+                    "descricao $index"
+                )
+            )
         }
     }
 }
