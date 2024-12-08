5b5947d9c311fba278673a95547db26cf0d13bc4
Gabriele K Silva <gabriele.s@aluno.ifsp.edu.br>
2024-12-08 16:14:54 -0300

    colocando modo de visualização para quando for modo de listagem os campos ficarem desabilitados e quando for inserir ou editar eles ficarem habilitados. Criando constant para levar os dados de uma tela para a outra

--- app/src/main/java/br/edu/scl/ifsp/ads/pdm/petlife/model/Constant.kt
+++ app/src/main/java/br/edu/scl/ifsp/ads/pdm/petlife/model/Constant.kt
@@ -3,6 +3,7 @@ package br.edu.scl.ifsp.ads.pdm.petlife.model
 object Constant {
     const val PARAMETRO_DADOS = "PARAMETRO_DADOS"
     const val EVENT_LIST = "EVENT_LIST"
+    const val ULTIMA_VISITA_VET = "ULTIMA_VISITA_VET"
     const val VIEW_MODE = "VIEW_MODE"
     const val PARAMETRO_VET = "PARAMETRO_VET"
     const val ID_EVENTO = -1
--- app/src/main/java/br/edu/scl/ifsp/ads/pdm/petlife/ui/UltimaVisitaVetActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/ads/pdm/petlife/ui/UltimaVisitaVetActivity.kt
@@ -2,10 +2,14 @@ package br.edu.scl.ifsp.ads.pdm.petlife.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.View.GONE
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.scl.ifsp.ads.pdm.petlife.R
 import br.edu.scl.ifsp.ads.pdm.petlife.databinding.ActivityUltimaVisitaVetBinding
+import br.edu.scl.ifsp.ads.pdm.petlife.model.Constant
 import br.edu.scl.ifsp.ads.pdm.petlife.model.Constant.EVENT_LIST
+import br.edu.scl.ifsp.ads.pdm.petlife.model.Constant.ULTIMA_VISITA_VET
 import br.edu.scl.ifsp.ads.pdm.petlife.model.Event
 
 class UltimaVisitaVetActivity : AppCompatActivity() {
@@ -18,8 +22,9 @@ class UltimaVisitaVetActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(auvet.root)
 
-
-        val receivedEvent = intent.getParcelableExtra<Event>(EVENT_LIST)
+        // se for falso ou vazio ele vai para o modo visualização e nao modo edicao
+        val viewMode = intent.getBooleanExtra(Constant.VIEW_MODE, false)
+        val receivedEvent = intent.getParcelableExtra<Event>(ULTIMA_VISITA_VET)
 
         receivedEvent?.let { ultimaVisitaVet ->
             with(auvet) {
@@ -59,10 +64,22 @@ class UltimaVisitaVetActivity : AppCompatActivity() {
                 eventoPet
             )
             Intent().apply {
-                putExtra(EVENT_LIST, event)
+                putExtra(ULTIMA_VISITA_VET, event)
                 setResult(RESULT_OK, this)
                 finish()
             }
+        }
+
+        //modo visualização
+        auvet.run {
+            dataEt.isEnabled = !viewMode
+            vetRb.isEnabled = !viewMode
+            vacinaRb.isEnabled = !viewMode
+            petshopRb.isEnabled = !viewMode
+
+
+            ultVisitBt.visibility = if (viewMode) GONE else View.VISIBLE
+
         }
     }
 }
